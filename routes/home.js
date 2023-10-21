866ae0390e4dbcced87c9ca920f4a134ebc3c2c8
André Custódio <andrecustodiotablet@gmail.com>
2023-10-21 22:22:39 +0100

    .

---
+++ routes/home.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require('path');
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  // return res.status(200).json({
+  //   title: "Express Testing",
+  //   message: "The app is working properly!",
+  // });
+});
+
+module.exports = router;
