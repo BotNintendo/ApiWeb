480468531c0ee6e61c8a2171e0d470aedca3c760
André Custódio <andrecustodiotablet@gmail.com>
2023-10-22 12:47:44 +0100

    .

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const home = require("./routes/home");
 
+let s = '';
+
 // Middlewares
 const app = express();
 app.use(express.json());
@@ -12,3 +14,15 @@ app.use("/home", home);
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
+
+app.post('/send', (req, res) => {
+    //res.json({ requestBody: req.body })  // <==== req.body will be a parsed JSON object
+
+    s = req.body;
+    console.log(s);
+})
+
+app.post('/received', (req, res) => {
+    res.json({ s })  // <==== req.body will be a parsed JSON object
+    s = "";
+})
